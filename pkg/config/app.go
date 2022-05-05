03f46983e84574020a4c398e695a4bf9efbdfd1a
Brahim Berkasse <berkassebrahim@gmail.com>
2022-05-05 13:08:54 +0000

    ✨ tests endpoints

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -10,7 +10,7 @@ var (
 )
 
 func Connect() {
-	d, err := gorm.Open("mysql", "root:root@/bookswithgo?charset=utf8&parseTime=True&loc=Local")
+	d, err := gorm.Open("mysql", "root:root@/booksapi?charset=utf8&parseTime=True&loc=Local")
 	if err != nil {
 		panic(err)
 	}
