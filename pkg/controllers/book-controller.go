b5a2a4e48bc4df439dab1375e692fb647d9579c8
Brahim Berkasse <berkassebrahim@gmail.com>
2022-05-05 12:17:17 +0000

    🦤 update and delete book controller functions !

--- pkg/controllers/book-controller.go
+++ pkg/controllers/book-controller.go
@@ -45,5 +45,44 @@ func CreateBook(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "pkglication/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write(res)
+}
 
+func DeleteBook(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	bookId := vars["bookId"]
+	ID, err := strconv.ParseInt(bookId, 0, 0)
+	if err != nil {
+		fmt.Println("Error accured whle parsing")
+	}
+	book := models.DeleteBook(ID)
+	res, _ := json.Marshal(book)
+	w.Header().Set("Content-Type", "pkglication/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func UpdateBook(w http.ResponseWriter, r *http.Request) {
+	book := &models.Book{}
+	utils.ParseBody(r, book)
+	vars := mux.Vars(r)
+	bookId := vars["bookId"]
+	ID, err := strconv.ParseInt(bookId, 0, 0)
+	if err != nil {
+		fmt.Println("Error accured while parsing !")
+	}
+	b, db := models.GetBookById(ID)
+	if book.Name != "" {
+		b.Name = book.Name
+	}
+	if book.Author != "" {
+		b.Author = book.Author
+	}
+	if book.Publication != "" {
+		b.Publication = book.Publication
+	}
+	db.Save(&b)
+	res, _ := json.Marshal(b)
+	w.Header().Set("Content-Type", "pkglication/jsonm")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
 }
--- pkg/routes/book-routes.go
+++ pkg/routes/book-routes.go
@@ -9,6 +9,6 @@ var RegisterBookStoreRoutes = func(router *mux.Router) {
 	router.HandleFunc("/book/", controllers.CreateBook).Methods("POST")
 	router.HandleFunc("/book/", controllers.GetBook).Methods("GET")
 	router.HandleFunc("/book/{bookID}", controllers.GetBookById).Methods("GET")
-	router.HandleFunc("/book/{bookID}", controllers.EditBook).Methods("PUT")
+	router.HandleFunc("/book/{bookID}", controllers.UpdateBook).Methods("PUT")
 	router.HandleFunc("/book/{bookID}", controllers.DeleteBook).Methods("DELETE")
 }
