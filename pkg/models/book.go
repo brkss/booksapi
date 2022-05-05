28913eef28d846105771467c6043379868503811
Brahim Berkasse <berkassebrahim@gmail.com>
2022-05-05 11:34:15 +0000

    🌾 setup model's functions 1

---
+++ pkg/models/book.go
@@ -0,0 +1,48 @@
+package models
+
+import (
+	"github.com/brkss/booksapi/pkg/config"
+	"github.com/jinzhu/gorm"
+)
+
+var db *gorm.DB
+
+type Book struct {
+	//gorm.model
+	Name        string `gorm:""json:"name"`
+	Author      string `json:"author"`
+	Publication string `json:"publication"`
+}
+
+func init() {
+	config.Connect()
+	db = config.GetDB()
+	db.AutoMigrate(&Book{})
+}
+
+func (b *Book) CreateBook() *Book {
+	db.NewRecord(b)
+	db.Create(&b)
+	return (b)
+}
+
+func GetAllBooks() []Book {
+	var books []Book
+
+	db.Find(&books)
+	return (books)
+}
+
+func GetBookById(ID int64) (*Book, *gorm.DB) {
+	var book Book
+
+	db := db.Where("ID=?", ID).Find(&book)
+	return &book, db
+}
+
+func DeleteBook(ID int64) Book {
+	var book Book
+
+	db.Where("ID=?", ID).Delete(book)
+	return book
+}
--- pkg/routes/book-routes.go
+++ pkg/routes/book-routes.go
@@ -1,15 +1,14 @@
 package routes
 
-
-import(
-	"github.com/gorilla/mux"
+import (
 	"github.com/brkss/booksapi/pkg/controllers"
+	"github.com/gorilla/mux"
 )
 
-var RegisterBookStoreRoutes = func(router *mux.Router){
-	router.HandleFunc("/book/", controllers.CreateBook).Methods('POST')
-	router.HandleFunc("/book/", controllers.GetBook).Method('GET')
-	router.HandleFunc("/book/{bookID}", controllers.GetBookById).Method('GET')
-	router.HandleFunc("/book/{bookID}", controllers.EditBook).Method('PUT')
-	router.HandleFunc("/book/{bookID}", controllers.DeleteBook).Method('DELETE')
+var RegisterBookStoreRoutes = func(router *mux.Router) {
+	router.HandleFunc("/book/", controllers.CreateBook).Methods("POST")
+	router.HandleFunc("/book/", controllers.GetBook).Methods("GET")
+	router.HandleFunc("/book/{bookID}", controllers.GetBookById).Methods("GET")
+	router.HandleFunc("/book/{bookID}", controllers.EditBook).Methods("PUT")
+	router.HandleFunc("/book/{bookID}", controllers.DeleteBook).Methods("DELETE")
 }
